aa6c37bdf9146d6dca7c81f96fc10d04230cebbd
albertosanchezc <albertosanchezc98@gmail.com>
2024-06-06 17:46:16 -0600

    cambio router devuelto

--- Router.php
+++ Router.php
@@ -24,10 +24,9 @@ class Router
         session_start();
 
         $auth = $_SESSION['login'] ?? null;
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         $method = $_SERVER['REQUEST_METHOD'];
 
-        $splitURL = explode('?', $currentUrl);
         //Arreglo de rutas protegidas
         $rutas_protegidas = [
             '/admin',
@@ -45,26 +44,27 @@ class Router
             '/tecnologias/eliminar'
         ];
 
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
         $metodo = $_SERVER['REQUEST_METHOD'];
 
 
         if ($metodo === 'GET') {
-            $fn = $this->rutasGet[$splitURL[0]] ?? null;
+            $fn = $this->rutasGet[$urlActual] ?? null;
         } else {
-            $fn = $this->rutasPOST[$splitURL[0]] ?? null;
+            $fn = $this->rutasPOST[$urlActual] ?? null;
         }
 
         //Proteger las rutas
-        // if (in_array($urlActual, $rutas_protegidas) && !$auth) {
-        //     header('Location: /');
-        // }
+        if (in_array($urlActual, $rutas_protegidas) && !$auth) {
+            header('Location: /');
+        }
 
 
         if ($fn) {
             //La URL existe y hay una funcion asociada
             call_user_func($fn, $this);
         } else {
-            echo "Pagina No Encontrada o Ruta no válida";
+            echo "Pagina No Encontrada";
         }
     }
 
